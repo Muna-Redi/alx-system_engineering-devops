31f45242ab159ce700620ce82c8c827754664838
Muna-Redi <francischym@gmail.com>
2023-01-11 18:01:34 -0800

    puppet-lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # This maniefest creates a file with given permissions and ownerships
 file {'/tmp/school/':
-  ensure => file,
-  path => '/tmp/school',
-  content => 'I love Puppet',
-  owner => 'www-data',
-  group => 'www-data',
-  mode => '0744'
+  ensure  => file,
+  path  => '/tmp/school',
+  content  => 'I love Puppet',
+  owner  => 'www-data',
+  group  => 'www-data',
+  mode  => '0744'
 }
