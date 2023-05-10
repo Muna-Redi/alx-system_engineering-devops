130f450db6f7d1179dd44eb4261f90809e3c89d4
Muna-Redi <francischym@gmail.com>
2023-05-10 03:47:05 +0100

    top ten hot subreddit posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" This script prints title of first top ten hot posts for
+    a given subreddit
+"""
+
+import json
+import requests
+
+
+def top_ten(subreddit):
+    """ Function prints titles of a subreddit's top ten hot posts """
+
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    user_agent = {
+        "User-Agent":
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101\
+        Firefox/15.0.1"
+    }
+    limit = {"limit": 10}
+    response = (requests.get(url, headers=user_agent, params=limit)).json()
+
+    try:
+
+        posts = response.requests.get("data").requests.get("children")
+        for post in posts:
+            print(post.requests.get("data").requests.get("title"))
+
+    except Exception as e:
+         print("None")
+
