ed134f5b96e154f80548d623aff0f39e0df8b345
Muna-Redi <francischym@gmail.com>
2023-05-11 03:08:30 +0100

    puppet script

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,8 @@
 # puppet script to fix an error in a line in a file on a server
 
 $file_name = '/var/www/html/wp-settings.php'
-$file_path = ['/bin','/usr/bin']
 
 exec { 'fix_line':
   command => "sed -i 's/phpp/php/g' ${file_to_edit}",
-  path    => "${file_path}"
+  path    => ['/bin','/usr/bin']
 }
